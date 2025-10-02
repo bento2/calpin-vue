f6be59f9dbe04993bd3940a34c0a46392e2b9d96
Laurent Bodelet <laurent.bodelet@gmail.com>
2025-10-02 15:58:55 +0200

    Mise à jour Exercice Card

--- src/types/ExerciceSeriesSchema.ts
+++ src/types/ExerciceSeriesSchema.ts
@@ -1,17 +1,18 @@
 import { z } from 'zod'
 import { SerieSchema } from '@/types/SerieSchema.ts'
-import { ExerciceSchema } from '@/types/ExerciceSchema.ts'
+import { addExerciceGetters, BaseExerciceSchema } from '@/types/ExerciceSchema.ts'
 
-export const ExerciceSeriesSchema = ExerciceSchema.extend({
+export const ExerciceSeriesSchema = BaseExerciceSchema.extend({
   series: z.array(SerieSchema).optional(),
-}).transform((data) => ({
-  ...data,
-  get completed() {
-    if (!data.series || data.series.length === 0) {
-      return false
-    }
-    return data.series.every((serie) => serie.checked === true)
-  },
-}))
+}).transform((data) => {
+  const base = addExerciceGetters(data)
+  return {
+    ...base,
+    get completed() {
+      if (!data.series || data.series.length === 0) return false
+      return data.series.every((serie) => serie.checked === true)
+    },
+  }
+})
 
 export type ExerciceSeries = z.infer<typeof ExerciceSeriesSchema>
