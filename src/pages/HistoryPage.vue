fc6e0e8f6c54dd51b0aa59a8861d68244d9f99f9
Laurent Bodelet <laurent.bodelet@gmail.com>
2025-11-03 15:53:28 +0100

    Mise à jour HistoryPage.vue

--- src/pages/HistoryPage.vue
+++ src/pages/HistoryPage.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { useSessionStore } from '@/stores/useSessionStore.ts'
 import { onMounted, ref } from 'vue'
-import type { Session } from '@/types/SessionSchema.ts'
+import { type Session } from '@/types/SessionSchema.ts'
 import SessionCard from '@/components/SessionCard.vue'
 
 const { getSessions } = useSessionStore()
@@ -10,12 +10,37 @@ const sessions = ref<Session[] | []>([])
 onMounted(async () => {
   getSessions().then((all) => {
     sessions.value = all ?? []
+    //recherche de la présence de sessions dans l'ancien store
+    /**const local = new LocalStorageAdapter<Session>()
+    let count = 0;
+    local.get('sessions').then((data) => {
+      if(Array.isArray(data)){
+        data.map((session: Session) => {
+          const index = sessions.value.findIndex((element) => element.id === session.id)
+          if(index === -1) {
+            count++;
+            //la session n'est pas dans la liste
+            sessions.value.push(SessionSchema.parse(session))
+          }
+        })
+      }
+      if(count !== 0){
+        persistSessions()
+      }
+    })**/
   })
+
+
 })
 </script>
 
 <template>
-  <SessionCard v-for="session in sessions" :key="session.id" :model-value="session" class="mb-4"></SessionCard>
+  <SessionCard
+    v-for="session in sessions"
+    :key="session.id"
+    :model-value="session"
+    class="mb-4"
+  ></SessionCard>
 </template>
 
 <style scoped></style>
